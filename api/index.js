388ec1da33bfa75f8f8a32eed2593de2038e9b25
Aman Sharma <amanketan11@gmail.com>
2024-05-31 20:44:23 +0530

    Connection to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
-
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
 const app = express();
+// console.log(process.env.MONGO)
+mongoose.connect(process.env.MONGO).then(() =>{console.log("connected")})
+
 
 app.get('/',(req,res)=>{
     res.send("hfdfddh")
